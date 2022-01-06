5b89ad1a096b15efe6b41b67789826a58e5f7448
EliMeitner <eli.meitner@toluna.com>
2022-01-06 15:20:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ resource "aws_s3_bucket_public_access_block" "codepipeline_bucket" {
   bucket = aws_s3_bucket.codepipeline_bucket.id
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
 }
 
 module "source_blue_green" {
