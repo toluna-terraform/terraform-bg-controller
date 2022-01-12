b59906ee78feb8afd363b9f6fa9dce90be937f29
EliMeitner <eli.meitner@toluna.com>
2022-01-12 12:00:03 +0200

    Update bb_functions.py and bitbucket_listener.py

--- modules/bitbucket_listener/lambda/bitbucket_listener.py
+++ modules/bitbucket_listener/lambda/bitbucket_listener.py
@@ -2,23 +2,11 @@ import json
 import logging
 import boto3
 import requests
-
+import bb_functions as bbf
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def bb_update_description(USERNAME,PASSWORD,pr_desc,pr_id,repo):
-    #url = "https://api.bitbucket.org/2.0/repositories/{}/pullrequests/{}".format(repo,pr_id)
-    #pr_desc 
-    #pr_desc += "# Test title\n\n| **Action** | **PR State** |\n| --- | --- |\n| PR  | Pending |\n|  |  |\n\n"
-    #data = {
-    #    "description":"{}".format(pr_desc)
-    #    
-    #}
-    #headers = {"Content-Type": "application/json"}
-    #response = requests.put(url, auth=("{}".format(USERNAME['Parameter']['Value']), "{}".format(PASSWORD['Parameter']['Value'])),data=json.dumps(data),headers=headers)
-    #response_body = response.json()
-    #logger.info("Response Code is::::" + str(response.status_code))
-    #logger.info("Response Body is::::" + str(response_body))
+
 
 def lambda_handler(event, context):
     ssm = boto3.client('ssm')
@@ -40,5 +28,5 @@ def lambda_handler(event, context):
         pr_desc = jsonpayload['pullrequest']['description']
         pr_id = jsonpayload['pullrequest']['id']
         repo = jsonpayload['repository']['full_name']
-        bb_update_description(USERNAME,PASSWORD,pr_desc,pr_id,repo)
+        bbf.bb_check_change_path(USERNAME,PASSWORD,pr_desc,pr_id,repo)
     return event
