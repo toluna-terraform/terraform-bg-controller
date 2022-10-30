48f6f626d51886bfddae999bad69c0e71d546e9d
EliMeitner <eli.meitner@toluna.com>
2022-10-30 07:39:15 +0200

    blue_green

--- modules/bg_controller/data.tf
+++ modules/bg_controller/data.tf
@@ -2,10 +2,18 @@ data "aws_iam_policy_document" "codebuild_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
-      type        = "Service"
-      identifiers = ["codebuild.amazonaws.com"]
-        }
+      type = "Service"
+      identifiers = [
+        "codepipeline.amazonaws.com",
+        "codedeploy.amazonaws.com",
+        "codebuild.amazonaws.com",
+        "cloudformation.amazonaws.com",
+        "iam.amazonaws.com",
+        "ssm.amazonaws.com",
+        "route53.amazonaws.com"
+      ]
     }
+  }
 }
 
 data "aws_route53_zone" "public" {
