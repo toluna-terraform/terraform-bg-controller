69b80f9bf4ddf72b8bf476748fc2b9ea3a94c81b
EliMeitner <eli.meitner@toluna.com>
2023-06-08 16:19:17 +0300

    add pipline trigger

---
+++ modules/pipeline_trigger/lambda/pipeline_trigger.js
@@ -0,0 +1,11 @@
+const AWS = require('aws-sdk');
+const cd = new AWS.CodeDeploy({ apiVersion: '2014-10-06', region: 'us-east-1' });
+const region = "us-east-1"
+
+exports.handler = async function (event, context, callback) {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  let bucketName = event.Records[0].s3.buckt.name;
+  let path = event.Records[0].s3.buckt.object.key;
+  let env_name = path.replace('/source_artifacts.zip','');
+  console.log(`TRIGGER: \n{\"PIPELINE\":\"codepipline-${process.env.APP_NAME}-${env_name}\"}`);
+}
---
+++ modules/pipeline_trigger/variables.tf
@@ -0,0 +1,8 @@
+variable "app_name" {
+  type = string
+}
+
+variable "env_type" {
+  type = string
+}
+
