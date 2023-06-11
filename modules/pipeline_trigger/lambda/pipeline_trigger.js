68e47ba902dd150a6a11e53322e553b1cfbb05f8
EliMeitner <eli.meitner@toluna.com>
2023-06-11 07:14:53 +0300

    Update lambda.zip and pipeline_trigger.js

--- modules/pipeline_trigger/lambda/pipeline_trigger.js
+++ modules/pipeline_trigger/lambda/pipeline_trigger.js
@@ -3,6 +3,7 @@ const codepipeline = new AWS.CodePipeline({ apiVersion: '2015-07-09' });
 const region = "us-east-1"
 
 exports.handler = async function (event, context, callback) {
+
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
   let bucketName = event.Records[0].s3.bucket.name;
   let path = event.Records[0].s3.object.key;
@@ -12,14 +13,14 @@ exports.handler = async function (event, context, callback) {
   var params = {
     name: `${pipeline_name}`
   };
-  codepipeline.startPipelineExecution(params, function (err, data) {
+  let pipeline_execution = await codepipeline.startPipelineExecution(params, function (err, data) {
     if (err) {
       console.log(`ERROR: Failed to start pipeline codepipeline-${process.env.APP_NAME}-${env_name}`);
       console.log(err, err.stack)
     }
     else {
       console.log(`STARTING PIPELINE: codepipeline-${process.env.APP_NAME}-${env_name}`)
-      console.log(data)
+      console.log("PIPELINE_EXECUTION: \n" +  JSON.stringify(data, null, 2))
     };
-  });
+  }).promise();
 }
