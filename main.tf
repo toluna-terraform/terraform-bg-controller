53921a34f860171e7952a3fc83ad1b959caf9a80
EliMeitner <eli.meitner@toluna.com>
2024-01-17 07:32:46 +0200

    config to controller

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
+locals {
+  app_name          = var.app_name == null ? var.controller_config.app_name : var.app_name
+  app_type          = var.app_type == null ? var.controller_config.app_type : var.app_type
+  apps              = var.apps == null ? var.controller_config.apps : var.apps
+  domain            = var.domain == null ? var.controller_config.domain : var.domain
+  env_type          = var.env_type == null ? var.controller_config.env_type : var.env_type
+  path_pattern      = var.path_pattern == null ? var.controller_config.path_pattern : var.path_pattern
+  source_repository = var.source_repository == null ? var.controller_config.source_repository : var.source_repository
+  aws_profile       = var.aws_profile == null ? var.controller_config.aws_profile : var.aws_profile
+  ttl               = var.ttl == null ? var.controller_config.ttl : var.ttl
+}
+
 resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket        = "s3-codepipeline-${var.app_name}-${var.env_type}"
+  bucket        = "s3-codepipeline-${local.app_name}-${local.env_type}"
   force_destroy = true
   tags = tomap({
     UseWithCodeDeploy = true
@@ -33,7 +45,7 @@ data "aws_caller_identity" "current" {
 
 provider "aws" {
   alias   = "prod"
-  profile = "${var.app_name}-prod"
+  profile = "${local.app_name}-prod"
 }
 
 data "aws_caller_identity" "prod" {
@@ -77,25 +89,25 @@ resource "aws_s3_bucket_acl" "source_codebuild_bucket" {
   bucket = aws_s3_bucket.codepipeline_bucket.id
   acl    = "private"
   depends_on = [
-    aws_s3_bucket.codepipeline_bucket, aws_s3_bucket_versioning.codepipeline_bucket,aws_s3_bucket_ownership_controls.source_codebuild_bucket
+    aws_s3_bucket.codepipeline_bucket, aws_s3_bucket_versioning.codepipeline_bucket, aws_s3_bucket_ownership_controls.source_codebuild_bucket
   ]
 }
 
 module "source_blue_green" {
-  for_each          = var.apps
+  for_each          = local.apps
   source            = "./modules/bg_controller"
   env_name          = each.key
-  app_name          = var.app_name
-  app_type          = var.app_type
+  app_name          = local.app_name
+  app_type          = local.app_type
   env_type          = each.value.env_type
-  path_pattern      = var.path_pattern
-  domain            = var.domain
-  aws_profile       = var.aws_profile
-  ttl               = var.ttl
+  path_pattern      = local.path_pattern
+  domain            = local.domain
+  aws_profile       = local.aws_profile
+  ttl               = local.ttl
   is_managed_env    = each.value.is_managed_env
   trigger_branch    = each.value.pipeline_branch
   pipeline_type     = each.value.pipeline_type
-  source_repository = var.source_repository
+  source_repository = local.source_repository
   bucket_id         = aws_s3_bucket.codepipeline_bucket.id
   depends_on = [
     aws_s3_bucket.codepipeline_bucket
@@ -104,22 +116,22 @@ module "source_blue_green" {
 
 module "merge_waiter" {
   source            = "./modules/merge_waiter"
-  app_name          = var.app_name
-  env_type          = var.env_type
-  source_repository = var.source_repository
+  app_name          = local.app_name
+  env_type          = local.env_type
+  source_repository = local.source_repository
 }
 
 module "notifier" {
-  count = var.env_type == "prod" ? 1 : 0
+  count             = local.env_type == "prod" ? 1 : 0
   source            = "./modules/notifier"
-  app_name          = var.app_name
-  env_type          = var.env_type
-  source_repository = var.source_repository
+  app_name          = local.app_name
+  env_type          = local.env_type
+  source_repository = local.source_repository
 }
 
 module "pipeline_trigger" {
   source     = "./modules/pipeline_trigger"
-  app_name   = var.app_name
-  env_type   = var.env_type
+  app_name   = local.app_name
+  env_type   = local.env_type
   depends_on = [aws_s3_bucket.codepipeline_bucket]
 }
--- variables.tf
+++ variables.tf
@@ -1,36 +1,63 @@
+variable "controller_config" {
+  #type = map(string)
+}
+
 variable "apps" {
+  type = map(object({
+    env_type        = string,
+    pipeline_branch = string,
+    pipeline_type   = string,
+    is_managed_env  = bool,
+    sq_enabled      = bool
+  }))
+  default  = null
+  nullable = true
 }
 
 variable "app_name" {
-  type = string
+  type     = string
+  default  = null
+  nullable = true
 }
 
 variable "path_pattern" {
-  type = string
+  type     = string
+  default  = null
+  nullable = true
 }
 
 variable "source_repository" {
-    type = string
+  type     = string
+  default  = null
+  nullable = true
 }
 
 variable "env_type" {
-    type = string
+  type     = string
+  default  = null
+  nullable = true
 }
 
 variable "domain" {
-    type = string
+  type     = string
+  default  = null
+  nullable = true
 }
 
 variable "app_type" {
-  default = "ecs"
-  type = string
+  type     = string
+  default  = null
+  nullable = true
 }
 
 variable "aws_profile" {
-  type = string
+  type     = string
+  default  = null
+  nullable = true
 }
 
 variable "ttl" {
-  type = number
-  default = 300
+  type     = number
+  default  = null
+  nullable = true
 }
