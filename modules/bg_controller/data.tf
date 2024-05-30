8962bdf2f01904de3f844369424bd2aecdc205c3
EliMeitner <eli.meitner@toluna.com>
2024-05-30 12:41:43 +0300

    remove redundent public hosted zone

--- modules/bg_controller/data.tf
+++ modules/bg_controller/data.tf
@@ -17,9 +17,4 @@ data "aws_iam_policy_document" "codebuild_assume_role_policy" {
   }
 }
 
-data "aws_route53_zone" "public" {
-  name         = var.domain
-  private_zone = false
-}
-
 data "aws_caller_identity" "current" {}
